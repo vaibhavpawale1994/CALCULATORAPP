638a718797c18ea9b35bbcbd4dcbdf0c9cd5700d
Vaibhav <vaibhavpawale19@gmail.com>
2021-04-16 10:56:24 +0530

    The basic calculator app using Ionic 4 and Angular 10

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { isNumber } from 'util';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  value = '0';
+  oldValue = '0';
+  lastOperator = 'x';
+  readyForNewInput = true;
+
+  numbersGroup = [
+    [7, 8, 9, 'x'],
+    [4, 5, 6, '/'],
+    [1, 2, 3, '+'],
+    [0, 'C', '%', '=']
+  ];
+
+  constructor() {}
+
+  onButtonPress(symbol) {
+    console.log(symbol);
+
+    if (isNumber(symbol)) {
+      console.log('is a number');
+      if (this.readyForNewInput)
+        this.value = '' + symbol;
+      else
+        this.value += '' + symbol;
+      this.readyForNewInput = false;
+    }
+    else if (symbol === 'C') {
+      this.value = '0';
+      this.readyForNewInput = true;
+    }
+    else if (symbol === '=') {
+      if (this.lastOperator === 'x')
+        this.value = '' + (parseInt(this.oldValue) * parseInt(this.value));
+      else if (this.lastOperator === '-')
+        this.value = '' + (parseInt(this.oldValue) - parseInt(this.value));
+      else if (this.lastOperator === '+')
+        this.value = '' + (parseInt(this.oldValue) + parseInt(this.value));
+      else if (this.lastOperator === '/')
+        this.value = '' + (parseInt(this.oldValue) / parseInt(this.value));
+      this.readyForNewInput = true;
+    }
+    else { // operator
+      this.readyForNewInput = true;
+      this.oldValue = this.value;
+      this.lastOperator = symbol;
+    }
+  }
+
+}
